cd412cffe48428a13e0ed0aa4a81f314d804000b
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-11 22:36:56 -0400

    Remove pre-made stuff, download emotion, setup container

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import styled from '@emotion/styled'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+    min-height: 100vh;
+  `;
+
   return (
-    <>
-    <div>
+    <Container>
       <h1>Crypto Stand</h1>
-    </div>
-    </>
+      <p>Available now on the app store.</p>
+      <p>Test your day-trader skills by participating in the cryptocurrency market.</p>
+      <p>Challenge your friends</p>
+      <p>Learn about cryptocurrency</p>
+      <p>Prove you are the best (without having to spend money)</p>
+    </Container>
   )
 }
 
