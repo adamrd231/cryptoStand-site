8d195beea1b92d143ae195f564725af8c850da4a
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-13 17:29:55 -0500

    Update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Container } from '../src/Components/Layouts'
+import { Container, RowContainer } from '../src/Components/Layouts'
 import styled from '@emotion/styled'
-
-const AppStoreButton = styled.button`
-  
-`;
 
 
 function App() {
@@ -15,12 +9,16 @@ function App() {
   return (
     <Container>
       <h1>Crypto Stand</h1>
-      <p>Available now on the app store.</p>
+      <p>Free app available now on the app store.</p>
       <p>Test your day-trader skills by participating in the cryptocurrency market.</p>
       <p>Challenge your friends</p>
       <p>Learn about cryptocurrency</p>
       <p>Prove you are the best (without having to spend money)</p>
-      <AppStoreButton>App Store</AppStoreButton>
+      <RowContainer>
+        <button>App Store</button>
+        <button>Contact me</button>
+      </RowContainer>
+     
 
     </Container>
   )
--- src/Components/Layouts.jsx
+++ src/Components/Layouts.jsx
@@ -1,11 +1,19 @@
 import styled from '@emotion/styled'
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: flex-start;
-width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+
 `;
 
-export { Container };
+const RowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export { Container,  RowContainer };
