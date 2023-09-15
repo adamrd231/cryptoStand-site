6472d37f94a1112c1ef12a8e21e87b17331b9c13
Adam Reed <adam.rdconcepts@gmail.com>
2023-09-15 10:26:49 -0600

    Add logo to menu, continue adjusting layout and adding options for containers to have parameters

--- src/Components/Layouts.jsx
+++ src/Components/Layouts.jsx
@@ -8,15 +8,19 @@ const Container = styled.div`
 
   min-height: 100vh;
   width: 100%;
+  box-sizing: border-box;
 `;
 
 const RowContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: ${ props => props.justify ? props.justify : 'flex-end' };
   align-items: center;
   width: 100%;
   background: ${ props => props.background ? props.background : 'transparent' };
+  gap: 1rem;
+  padding: 0.5rem;
+  box-sizing: border-box;
 `;
 
 const ColumnContainer = styled.div`
--- src/Views/Menu.jsx
+++ src/Views/Menu.jsx
@@ -1,5 +1,20 @@
 import styled from "@emotion/styled";
 import { RowContainer } from "../Components/Layouts";
+import LogoImage from '../assets/CryptoLogo.png'
+
+const Logo = styled.img`
+  height: 3rem;
+  width: 3rem;
+  background: #333;
+  border-radius: 1rem;
+  padding: 0.5rem;
+`;
+
+const LogoTitle = styled.h2`
+  font-weight: 700;
+  font-size: 1rem;
+  color: #efefef;
+`;
 
 const MenuItem = styled.div`
 
@@ -8,9 +23,10 @@ const MenuItem = styled.div`
 function Menu() {
   return (
     <RowContainer>
-      <div>
-        <MenuItem>Logo</MenuItem>
-      </div>
+      <RowContainer justify='flex-start'>
+         <Logo src={LogoImage}></Logo>
+         <LogoTitle>Crypto Stand</LogoTitle>
+      </RowContainer>
       <RowContainer>
         <MenuItem>Contact</MenuItem>
         <MenuItem>FAQ</MenuItem>
